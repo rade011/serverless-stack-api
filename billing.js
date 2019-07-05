bda8a3ad8fc0e0822ca7b26bc1297c5d66df7fdd
rade-chainapp-tech <rade.radivojevic@gmail.com>
2019-07-05 15:54:13 +0200

    Add unit testing
    * Create billing test in test folder
    
    * Update serverless.yml file for env variables create billing-event.json file in mocks

---
+++ billing.js
@@ -0,0 +1,27 @@
+import stripePackage from "stripe";
+import { calculateCost } from "./libs/billing-lib";
+import { success, failure } from "./libs/response-lib";
+
+const util = require("util");
+
+export async function main(event, context) {
+  const { storage, source } = JSON.parse(event.body);
+  const amount = calculateCost(storage);
+  const description = "Scratch charge";
+
+  // Load our secret key from the  environment variables
+  const stripe = stripePackage(process.env.stripeSecretKey);
+  const objectName = process.env.stripeSecretKey;
+
+  try {
+    await stripe.charges.create({
+      source,
+      amount,
+      description,
+      currency: "usd"
+    });
+    return success({ status: true });
+  } catch (e) {
+    return failure({ message: e.message });
+  }
+}
